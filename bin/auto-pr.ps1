09524aaf1686cc10f53fc9115d36fbe6b16713c0
Andrey D. <49348855+turdusMerul@users.noreply.github.com>
2025-11-01 20:17:41 +0300

    Remove comment on upstream parameter in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,8 @@
 param(
-    # overwrite upstream param
     [String]$upstream = "turdusMerul/scoop-custom-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+$dir = "$PSScriptRoot/../bucket"
 & $autopr -Dir $dir -Upstream $Upstream @Args
